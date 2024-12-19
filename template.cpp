0ac0a332ca61f12ce50ae0db51b49bb5bc2e3d65
YuldShah <notyuldshah@gmail.com>
2024-12-19 19:32:13 +0500

    setup

---
+++ template.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+// #define TEST
+// #define IOFILE
+#define iamspeed ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vii vector<pair<int, int>>
+#define fr(x) for(int i=0; i<x; i++)
+#define ll long long
+#define endl '\n'
+
+using namespace std;
+
+void solve();
+
+int main(){
+	
+	iamspeed
+
+	#ifdef IOFILE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	int t=1;
+	
+	#ifdef TEST
+		cin>>t;
+	#endif
+
+	while (t--)
+		solve();
+
+    return 0;
+}
+
+void solve(){
+	// Here goes my code
+}
